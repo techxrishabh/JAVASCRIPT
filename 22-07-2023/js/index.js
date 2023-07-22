20d5642fd32bae0f3e5f4dbd3a0ed3bd7096f3d8
rishabh22204901 <rishabh22204901@gmail.com>
2023-07-22 12:10:15 +0530

    day 2 done

---
+++ 22-07-2023/js/index.js
@@ -0,0 +1,14 @@
+//document.write("Hello-World")
+//alert("Hello-World")
+//console.log("Hello-World")
+
+//=====variable======
+let name = "Rishabh";
+console.log(name);
+
+let address, zip, state;
+address = "106 main street";
+zip = "901123";
+state = "CA";
+
+console.log(address, zip, state);
