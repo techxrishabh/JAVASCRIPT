e20082f64e80a4797ea63321f9e2d9af74421dd8
rishabh22204901 <rishabh22204901@gmail.com>
2023-07-28 12:01:12 +0530

    conditionals-challenge done

--- 27-07-2023/js/index.js
+++ 27-07-2023/js/index.js
@@ -212,6 +212,22 @@ if (value) {
   console.log("sorry world");
 }
 
+let num1 = 5;
+let num2 = 10;
+
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+} else {
+  console.log("num2 is greater than num1");
+}
+
+//      =========  !  ==========
+
+const value2 = false;
+if (!value2) {
+  console.log("its true");
+}
+
 // 1. create two objects "person1", "person2"
 // 2. setup name,age (15-25),
 //    status ('resident', 'tourist') keys
@@ -233,7 +249,14 @@ let person2 = {
   status: "resident",
 };
 
-if person1.age > 18 && 
+if (
+  (person1.age > 18 && person1.status == "tourist") ||
+  (person2.age > 18 && person2.status == "tourist")
+) {
+  console.log("True");
+} else {
+  console.log("False");
+}
 
 // ## Strings #8
 
