dd1c0a7f6c3ed7569bfb2931df51b883d2403984
rishabh22204901 <rishabh22204901@gmail.com>
2023-08-24 11:30:42 +0530

    get-element-by-class-name-done

---
+++ DOM/4/script.js
@@ -0,0 +1,10 @@
+// select the element or group of element that we want
+//decide the effect we want to apple to the selection
+
+//getElementByClassName('classname');
+
+//HTMLCollection
+
+const listItems = document.getElementsByClassName("special");
+listItems[0].style.color = "red";
+console.log(listItems);
