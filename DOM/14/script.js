6a37e80acf03703c7ea19662af6976bdec2a3414
rishabh22204901 <rishabh22204901@gmail.com>
2023-08-28 14:03:20 +0530

    added some changes

---
+++ DOM/14/script.js
@@ -0,0 +1,37 @@
+// // // // | createElement('element')
+// // // // | createTextNode('text content')
+// // // // | element.appendChild(childElement)
+// // // // | replaceChild("new", "old")
+
+//
+
+//
+// ==========   ========       ==============
+// CREATING DIV ELEMENT WITH TEXT  TO INSERT  IT INSIDE BODY ===========
+// ===========================
+//
+
+//selecting the id first
+let result = document.querySelector("#result");
+let first = document.querySelector("#red");
+//creating the element
+const bodyDiv = document.createElement("div");
+//creating the text node that we want to insert inside our newly created div
+const text = document.createTextNode("let we friends and friend together");
+//placing our newly created text inside our newly created div
+bodyDiv.appendChild(text);
+//now let insert it inside the body element before the result tagged element
+document.body.insertBefore(bodyDiv, result);
+
+//
+// ==========   ========       ==============
+// CREATING H2 ELEMENT WITH TEXT  TO INSERT  IT INSIDE RESULT TAGGED ELEMENT ===========
+// ===========================
+//
+
+//CREATING THE H2 ELEMENT
+const heading = document.createElement("h2");
+const textValue = heading.createTextNode("dynamic heading");
+heading.appendChild(textValue);
+heading.classList.add("blue");
+result.insertBefore(heading, first);
