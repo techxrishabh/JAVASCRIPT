2d3f3ec5363927f8909572b0c67d75e04d702061
rishabh22204901 <rishabh22204901@gmail.com>
2023-08-29 22:00:26 +0530

    remove( ) done

---
+++ DOM/16/removeChild === 2/script.js
@@ -0,0 +1,8 @@
+// remove()
+// removeChild
+
+//lets first select the id that we want to remove
+const result = document.querySelector("#result");
+// now lets try to remove that selected id
+result.remove();
+// now it has removed
