e8373ff3c446fdbd461763d308505e8c4bde3438
rishabh22204901 <rishabh22204901@gmail.com>
2023-09-02 21:11:50 +0530

    innerHTML and textContent done

--- DOM/17/script.js
+++ DOM/17/script.js
@@ -1,4 +1,23 @@
 // innerHTML
 // textContent
 
-//selecting the parent element of ul
+//selecting the parent element of li
+const list = document.getElementById("first");
+//selecting the div whose id is second
+const div = document.getElementById("second");
+//selecting the first child element of ul
+const item = document.querySelector(".item");
+
+// =================== text-content === by using this we can see what text is contained in the div element ====
+console.log(div.textContent);
+
+// =================== innerHTML === by using this we can see whole html
+console.log(list.innerHTML);
+console.log(list.textContent);
+
+const randomVar = "random value";
+
+const ul = document.createElement("ul");
+ul.innerHTML = `<li class="item">${randomVar}</li>
+      <li>list item</li> <li>list item</li>`;
+document.body.appendChild(ul);
