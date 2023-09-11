2951c260e2676a7d33a14fed9f5b8f4dc29ecaea
rishabh22204901 <rishabh22204901@gmail.com>
2023-09-11 22:53:55 +0530

    Event - Resize done

---
+++ DOM/36/script.js
@@ -0,0 +1,11 @@
+/* 
+The resize event fires when the document view (window) has been resized 
+*/
+
+window.addEventListener("resize", function () {
+  console.log(window.innerWidth);
+});
+
+/// by using this let see what will happen
+
+/// whenever we resize our windows this function will got activated =================
