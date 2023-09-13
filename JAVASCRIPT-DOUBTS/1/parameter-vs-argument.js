a18ebc02f8a33d8b9e1d52929e6dce53bf9d9e65
rishabh22204901 <rishabh22204901@gmail.com>
2023-09-13 21:42:07 +0530

    parameter-vs-argument-done

---
+++ JAVASCRIPT-DOUBTS/1/parameter-vs-argument.js
@@ -0,0 +1,18 @@
+// WHAT IS THE DIFFERENCE BETWEEN PARAMETER AND ARGUMENTS ?
+
+//__________________________ANSWER_________________________________;
+
+// function add (parameter1 , parameter2 ) {
+//   return parameter1 + parameter2;
+// }
+
+// add(10 (argument 1), 20 (argument2 ))
+
+// parameter is a variable passed to a function
+// The arguments are the actual values to variable that we pass in the function
+
+function addSum(a, b) {
+  return a + b;
+}
+
+console.log(addSum(10, 20));
